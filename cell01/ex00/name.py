58472b0759caac823538aab0214ab80c6ea375fe
Itsaraphong Watcharaprateep <iwatchar@z2t16c3.42bangkok.com>
2025-05-05 17:48:54 +0700

    cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,7 @@
+first_name = "Itsaraphong"
+last_name = "Watcharaprateep"
+def name(first_name, last_name):
+    first_name = "Itsaraphong"
+    last_name = "Watcharaprateep"
+    print(f"{first_name} {last_name}") 
+name(first_name, last_name)
