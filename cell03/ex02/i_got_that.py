d49dd123477410739852ce4e592e1051a059673e
Itsaraphong Watcharaprateep <iwatchar@z2t1c2.42bangkok.com>
2025-05-06 17:33:16 +0700

    cell03

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+while True:
+    num = input()
+    if num == "STOP":
+        print("I got that! Anything else? : " + num)
+        break
+    else :
+        print("I got that! Anything else? : " + num)
