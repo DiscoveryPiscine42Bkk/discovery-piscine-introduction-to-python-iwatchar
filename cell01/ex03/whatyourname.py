32975b52f229528eaf09a171631a4050dcb27be4
Itsaraphong Watcharaprateep <iwatchar@z2t1c2.42bangkok.com>
2025-05-06 17:27:23 +0700

    cell02

---
+++ cell01/ex03/whatyourname.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+first_name = input("Enter your name? : ")
+last_name = input ("Enter your last name? : ")
+print ("Hey, what's your first name? : " + first_name)
+print ("And your last name? : " + last_name)
+print ("Well, pleased to meet you "  f"{first_name} {last_name}")
